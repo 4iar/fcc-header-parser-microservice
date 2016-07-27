e8c57fe5f4963ebca65d3529f80abd05c88607a3
4iar <4iar@users.noreply.github.com>
2016-07-27 16:41:49 +0100

    Add express index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require('express');
+
+
+const app = express();
+
+// let Heroku set the port
+const port = process.env.PORT || 8080;
+
+app.get('/', (request, response) => {
+  response.json({
+    ipaddress: request.connection.remoteAddress,
+    language: request.headers['accept-language'].split(',')[0],
+    useragent: request.headers['user-agent'],
+    software: request.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+  });
+});
+
+app.listen(port);
