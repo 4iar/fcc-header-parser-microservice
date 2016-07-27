571f84b0e960999dae91cab3386cd863b623a0c7
4iar <4iar@users.noreply.github.com>
2016-07-27 17:22:31 +0100

    Handle malformed or empty user agents

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (request, response) => {
+  
+  // Handle malformed user agents e.g. that do not contain software or are undefined
+  let software = null;
+  const useragent = request.headers['user-agent'] ? request.headers['user-agent'] : null;
+  const softwareMatch = useragent ? request.headers['user-agent'].match(/\(([^)]+)\)/) : null
+  if (softwareMatch && softwareMatch.length > 1) {
+    software =  softwareMatch[1];
+  } 
+
   response.json({
     ipaddress: request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'].split(',')[0] : request.connection.remoteAddress,
     language: request.headers['accept-language'] ? request.headers['accept-language'].split(',')[0] : null,
-    useragent: request.headers['user-agent'],
-    software: request.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+    useragent,
+    software
   });
 });
 
