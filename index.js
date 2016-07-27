9c71777c044d3558d931ba90205bf430fdf6b351
4iar <4iar@users.noreply.github.com>
2016-07-27 16:54:46 +0100

    Fix ip parsing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (request, response) => {
   response.json({
-    ipaddress: request.connection.remoteAddress,
+    ipaddress: request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'].split(',')[0] : request.connection.remoteAddress,
     language: request.headers['accept-language'].split(',')[0],
     useragent: request.headers['user-agent'],
     software: request.headers['user-agent'].match(/\(([^)]+)\)/)[1]
